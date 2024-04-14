73f021e9e15536ef07f98b8f63fa5516d9ee99be
Juliano <162523773+JulianoMRA@users.noreply.github.com>
2024-04-14 15:34:08 -0300

    Botão Logout

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+
     path('', TaskList.as_view(), name='tasks'),
     path('register/', RegisterPage.as_view(), name='register'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
